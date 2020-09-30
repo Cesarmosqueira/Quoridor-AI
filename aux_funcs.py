53f30649b1551cf4aac4df0f482049bd37dfd3b2
Cesar Mosqueira <cesarmanuel.2802@gmail.com>
2020-09-30 02:49:37 -0500

    Movimiento Naranja

--- aux_funcs.py
+++ aux_funcs.py
@@ -11,7 +11,6 @@ def valid_block(board, r, c):
     dy = [0,0,1,-1]
     n, m = len(board), len(board[0])
 
-
     vis = [[False for i in range(m)] for j in range(n)]
 
     def valid(row, col):
@@ -48,9 +47,10 @@ mat =   [['#','#','#','#','#','#','#'],
          ['#','#','#','#','#','#','#']]
 
 
-dx = [1, -1, 0, 0]
+dx = [1, -1, 0, 0] ## R L D U
 dy = [0, 0, 1, -1]
-
+           #       (UR UL DR DL)
+diagonals = [(-1,1), (-1,-1), (1,1), (1,-1)]
 
 def get_next_move(board, side, startpoint): ## side == True = UP else DOWN 
     n, m = len(board), len(board[0])
@@ -66,24 +66,34 @@ def get_next_move(board, side, startpoint): ## side == True = UP else DOWN
     
 
     def valid(row, col):
-        return 0 <= row and row < n and 0 <= col and col < m and (board[row][col] == ' ' or board[row][col] == 'W')\
-             and move_reg[row][col] == -1 and board[row][col] != '#'
+        return 0 <= row and row < n and 0 <= col and col < m and move_reg[row][col] == -1 and board[row][col] != '#'
 
     def reconstruct_path(r, c):
         last = [r,c]
         while move_reg[r][c] >= 0:
             i = move_reg[r][c] 
-            r -= dx[i]
-            c -= dy[i]
+            if i < 4:
+                r -= dx[i]
+                c -= dy[i]
+            elif i < 8:
+                print(i)
+                r -= 2*dx[i%4]
+                c -= 2*dy[i%4]
+            else: 
+                i %= 4
+                r -= diagonals[i][0]
+                c -= diagonals[i][1]
             if (r,c) == startpoint:
                 return last
             else: last = [r-1,c-1]
-        return -1
+        print("wtf")
+        return -1, -1
             
 
     while len(q):
         r, c = q.popleft()
         if board[r][c] == 'W':
+            print(move_reg[r][c])
             if side: board[-1] = original
             else: board[0] = original
             return reconstruct_path(r,c)
@@ -91,8 +101,37 @@ def get_next_move(board, side, startpoint): ## side == True = UP else DOWN
         for i in range(4):
             nr, nc = r + dx[i], c + dy[i]
             if valid(nr, nc):
-                move_reg[nr][nc] = i
-                q.append((nr, nc))
+                if board[nr][nc] == 'X' or board[nr][nc] == 'O':    
+                    if(valid(nr+dx[i],nc+dy[i])):
+                        print("Im doing douyblke at", board[nr+dx[i]][nc+dy[i]])
+                        q.append((nr+dx[i],nc+dy[i]))
+                        move_reg[nr+dx[i]][nc+dy[i]] = i+4
+                    else:
+                        #      (UR UL DR DL)
+                        #  (-1,1), (-1,-1), (1,1), (1,-1)
+                        if dx[i] == 0:
+                            if valid(r+dy[i],c+1):      
+                                q.append((r+dy[i],c+1))  
+                                move_reg[r+dy[i]][c+1] = 8 + (0 if dy[i] == -1 else 2)
+
+                            if valid(r+dy[i],c-1):      
+                                q.append((r+dy[i],c-1))    
+                                move_reg[r+dy[i]][c-1] = 8 + (1 if dy[i] == -1 else 3)
+
+                        if dy[i] == 0:
+                            if valid(r+1,c+dx[i]): 
+                                q.append((r+1,c+dx[i]))
+                                move_reg[r+1][c+dx[i]] = 8 + (3 if dx[i] == -1 else 2)
+
+                            if valid(r-1,c+dx[i]): 
+                                q.append((r-1,c+dx[i]))
+                                move_reg[r-1][c+dx[i]] = 8 + (1 if dx[i] == -1 else 0)
+
+                else:
+                    move_reg[nr][nc] = i
+                    q.append((nr, nc))
+       
+
     print("WTF")
     return -1,-1
 
