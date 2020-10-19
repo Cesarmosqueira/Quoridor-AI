bc7a8a307c5c8611725d4c777753c5b40d9f7316
cesarmosqueira <cesarmanuel.2802@gmail.com>
2020-10-19 03:13:58 -0500

    Indices de movimiento en backend
    Ambos componentes de la matriz de backend estan funcionando pero falta
    implementar BFS modificado con esta version
    
    El movimiento está comentado

--- game.py
+++ game.py
@@ -17,12 +17,10 @@ class Board:
                 self.turn = {True:-1,False:1}[rnd.randint(0,1) == 0]            
                 self.mouse_index = [-1,-1]
                 ####matrix#####
-                self.backend = [[{True:'#', False:' '}[x==0 or x ==cols+1] for x in range(cols+2)]]*int(rows+2)
-                a = ['#']*(rows+2)
+                self.backend = [[{True:('#',5), False:(' ',0)}[x==0 or x ==cols+1] for x in range(cols+2)]]*int(rows+2)
+                a = [('#',5)]*(rows+2)
                 self.backend[0] = a; self.backend[-1] = a
-
         def mouse_shadow(self, screen, pos):
-            ## ifcursor is in fence poisition
             p = 0.14
             if not (self.h*p < pos[1]%self.h and pos[1]%self.h < self.h*(1-p)):
                 col, row = pos[0]//self.w, pos[1]//self.h
@@ -48,41 +46,24 @@ class Board:
                 pg.display.set_caption("None")
                 self.mouse_index = (int(-1), int(-1))
                 return
-
-            self.mouse_rect = pg.Rect(col*self.w+p, row*self.h+p, self.w-p*2, self.h-p*2)
-            self.valid_rect = None
-            #### blocks that can't be the same index as any of the players ####
-            """
-            if (row, col) != self.p1.get_col_row() and (row, col) != self.p2.get_col_row():
-                    pg.draw.rect(screen, Cregshadow, self.mouse_rect)
-                    self.valid_rect = self.mouse_rect
-                    self.mouse_index = (int(row),int(col))
-            else: self.mouse_index = [-1,-1]
-            """
-            
-            pg.draw.rect(screen, Cregshadow, self.mouse_rect)
-            self.valid_rect = self.mouse_rect
-            self.mouse_index = (int(row),int(col))
-            
-
-
+        
         def click(self, screen):
                 # turn
-                if self.valid_rect and self.mouse_index != self.p1.get_col_row() and self.mouse_index != self.p2.get_col_row():
-                        pg.draw.rect(screen,Cblack,(self.valid_rect))
-                        prev = self.backend[self.mouse_index[0]+1] 
-                        if valid_block(self.backend,self.mouse_index[0]+1, self.mouse_index[1]+1):
-                                self.backend[self.mouse_index[0]+1] = [' ' if x != self.mouse_index[1]+1 and prev[x] != '#' else '#' for x in range(int(self.cols+2))]
-                                self.backend[self.mouse_index[0]+1][0] = '#'; self.backend[self.mouse_index[0]+1][-1] = '#'
-
+                #if self.valid_rect and self.mouse_index != self.p1.get_col_row() and self.mouse_index != self.p2.get_col_row():
+                #        pg.draw.rect(screen,Cblack,(self.valid_rect))
+                #        prev = self.backend[self.mouse_index[0]+1] 
+                #        if valid_block(self.backend,self.mouse_index[0]+1, self.mouse_index[1]+1):
+                #                self.backend[self.mouse_index[0]+1] = [' ' if x != self.mouse_index[1]+1 and prev[x] != '#' else '#' for x in range(int(self.cols+2))]
+                #                self.backend[self.mouse_index[0]+1][0] = '#'; self.backend[self.mouse_index[0]+1][-1] = '#'
+                return
         def update_players(self, screen, w, h):
                 # draw pawns
                 pg.draw.circle(screen, Cpawn1, self.p1.get_coord(screen, w, h), abs(int(w/2) - 5))
                 pg.draw.circle(screen, Cpawn2, self.p2.get_coord(screen, w, h), abs(int(w/2) - 5))
                 pos1, pos2 = self.p1.get_col_row(), self.p2.get_col_row()
-                self.backend[pos1[0]+1] = ['X' if x == pos1[1]+1 else str(self.backend[pos1[0]+1][x]) for x in range(int(self.cols+2))]
-                self.backend[pos2[0]+1] = ['O' if x == pos2[1]+1 else str(self.backend[pos2[0]+1][x]) for x in range(int(self.cols+2))]
-
+                self.backend[pos1[0]+1] = [('X',self.backend[pos1[0]+1][i][1]) if i == pos1[1]+1 else self.backend[pos1[0]+1][i] for i in range(len(self.backend[pos1[0]+1]))]
+                self.backend[pos2[0]+1] = [('O',self.backend[pos2[0]+1][i][1]) if i == pos2[1]+1 else self.backend[pos2[0]+1][i] for i in range(len(self.backend[pos2[0]+1]))]
+                return
         def draw_board(self, screen, w, h, p):
                 # rectangles
                 for i in range(self.rows):
@@ -93,11 +74,13 @@ class Board:
                                         pg.draw.rect(screen, Cblack, (i*w, j*h, w, h))
 
         def move_pawns(self):
-                if self.turn == 1: self.p1.next_move(self.backend)
-                else: self.p2.next_move(self.backend)
+                for i in self.backend: print(i)
+                print("\n\n")
+                self.moveflag = True
+                #if self.turn == 1: self.p1.next_move(self.backend)
+                #else: self.p2.next_move(self.backend)
                 self.turn *= -1
-
-
+        
         def update_board(self, screen):
                 w, h, p = self.w, self.h, 1
                 self.draw_board(screen, w, h, p)
